a76c6d4ac9bb361d858aaf9b7581df696115a0ff
wendersonn.freitas <1043002Xbc>
2021-02-17 20:14:59 -0300

    iniciando projeto

---
+++ src/main/java/com/idrust/bmfpriceAPI/BmfPriceApiApplication.java
@@ -0,0 +1,13 @@
+package com.idrust.bmfpriceAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmfPriceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmfPriceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idrust/bmfpriceAPI/BmfPriceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idrust.bmfpriceAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmfPriceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
